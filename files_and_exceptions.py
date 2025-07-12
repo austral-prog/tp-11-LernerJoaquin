71781ca6108ad3586090ddb5af5bc173e3881c73
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-12 18:19:51 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,1 +1,13 @@
-
+def process_dict(data):
+    """Para cada producto, imprime el total de ventas y el promedio, en el orden natural del diccionario.
+    :param data: dict - diccionario a procesar.
+    :return: None"""
+    suma = 0
+    divicion = 0
+    for producto in data.keys():
+        for valor in range(len(data[producto])):
+            divicion +=1
+            suma += data[producto][valor]
+        print (f"{producto}: ventas totales ${suma:.2f}, promedio ${(suma/divicion):.2f}")
+        divicion = 0
+        suma = 0
