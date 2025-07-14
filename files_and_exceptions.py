ffb335a59dca2521329a2d86794ac14a9fec1dd8
LernerJoaquin <lernerjoaquin@gmail.com>
2025-07-14 17:42:03 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,8 +1,4 @@
 def process_dict(data):
-    """Para cada producto, imprime el total de ventas y el promedio, en el orden natural del diccionario.
-    :param data: dict - diccionario a procesar.
-    :return: None
-    """
     suma = 0
     divicion = 0
     for producto in data.keys():
